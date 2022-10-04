49bf9583001f60b57bccd463a7f64442173ea26a
Field Palmer <gfieldpalmer@gmail.com>
2022-10-03 23:16:53 -0600

    lookin better workin better

--- src/App.jsx
+++ src/App.jsx
@@ -23,16 +23,16 @@ const App = () => {
             <Routes>
               <Route path="/" element={<Discover />} />
               <Route path="/top-artists" element={<TopArtists />} />
-              <Route path="/top-charts" element={<TopCharts />} />
+              {/* <Route path="/top-charts" element={<TopCharts />} /> */}
               <Route path="/around-you" element={<AroundYou />} />
               <Route path="/artists/:id" element={<ArtistDetails />} />
               <Route path="/songs/:songid" element={<SongDetails />} />
               <Route path="/search/:searchTerm" element={<Search />} />
             </Routes>
           </div>
-          {/* <div className="xl:sticky relative top-0 h-fit">
+          <div className="xl:sticky relative top-0 h-fit">
             <TopPlay />
-          </div> */}
+          </div>
         </div>
       </div>
 
